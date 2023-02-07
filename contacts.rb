44428ab4b6325affdb33ec8c9903a8fb2607d6ac
kimathinjoki <dfkimathinjoki@gmail.com>
2023-02-06 23:45:26 -0800

    Completed

--- contacts.rb
+++ contacts.rb
@@ -9,8 +9,8 @@ def contacts
       email: "jon_snow@thewall.we",
       favorite_ice_cream_flavors: ["chocolate", "vanilla"]
     },
-    "Freddie Mercury" => {
-      name: "Freddie",
+    "Freddy Mercury" => {
+      name: "Freddy",
       email: "freddie@mercury.com",
       favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
     }
@@ -20,12 +20,11 @@ end
 def remove_strawberry(contacts)
   # your code here!
   contacts.each do |person, contact_details_hash|
-    if person == "Freddie Mercury"
+    if person == "Freddy Mercury"
       contact_details_hash.each do |attribute,data|
 
        if attribute == :favorite_ice_cream_flavors
         data.delete_if {|ice_cream| ice_cream =="strawberry"}
-       
        end
        
       end
@@ -37,3 +36,20 @@ end
 
 # print the output to the terminal for inspection
 pp remove_strawberry(contacts)
+
+
+# quiet_and_loud = ["hi", "HI", "shhh", "WHAT?!"]
+
+# quiet_and_loud.find do |word|
+#   word == word.upcase 
+# end
+
+# puts quiet_and_loud
+
+
+# lunch_menu = ["pizza", "sandwich", "sushi", "soup", "salad"]
+
+# lunch_menu.each do |lunch_item|
+#   puts "#{lunch_item}!"
+# end
+
